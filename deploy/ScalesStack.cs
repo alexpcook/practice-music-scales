b3778de4eae72d055eaab3f33a58346e2344ae85
Alex Cook <apcook7104@gmail.com>
2020-12-06 15:27:39 -0600

    Add s3 bucket for static site objects

--- deploy/ScalesStack.cs
+++ deploy/ScalesStack.cs
@@ -4,6 +4,7 @@ using Pulumi.Aws.Iam;
 using Pulumi.Aws.Lambda;
 using Pulumi.Aws.ApiGateway;
 using Pulumi.Aws.S3;
+using Pulumi.Aws.S3.Inputs;
 
 class ScalesStack : Stack
 {
@@ -27,7 +28,22 @@ class ScalesStack : Stack
 
         var s3 = new Bucket("s3bucket", new BucketArgs
         {
-            Acl = "private",
+            BucketName = "scalesapi",
+            Website = new BucketWebsiteArgs
+            {
+                IndexDocument = "site.html",
+                // todo - add error document
+            },
+        });
+
+        var s3policy = new BucketPolicy("s3policy", new BucketPolicyArgs
+        {
+            Bucket = s3.BucketName,
+            Policy = GetS3PublicReadPolicy(),
+        }, new CustomResourceOptions
+        {
+            DependsOn = { s3 },
+            Parent = s3,
         });
 
         foreach (string fileType in StaticFileTypes)
@@ -35,7 +51,7 @@ class ScalesStack : Stack
             string id = "s3object-" + fileType;
             _ = new BucketObject(id, new BucketObjectArgs
             {
-                Key = id,
+                Key = Output.Format($"site.{fileType}"),
                 Bucket = s3.Id,
                 Source = new FileAsset(StaticContentDirectory + fileType + "/site." + fileType),
                 ContentType = (fileType == "js") ? "text/javascript" : "text/" + fileType,
@@ -50,6 +66,51 @@ class ScalesStack : Stack
             Name = "practice-music-scales",
             Description = "an api gateway that returns musical scales in a random order",
             Policy = GetApiGatewayPolicy(),
+        });
+
+        var rootResource = new Pulumi.Aws.ApiGateway.Resource("resource-root", new Pulumi.Aws.ApiGateway.ResourceArgs
+        {
+            RestApi = gateway.Id,
+            PathPart = "{bucketkey}",
+            ParentId = gateway.RootResourceId,
+        }, new CustomResourceOptions
+        {
+            DependsOn = { gateway },
+            Parent = gateway,
+        });
+
+        var rootResourceMethod = new Method("method-get", new MethodArgs
+        {
+            HttpMethod = "GET",
+            Authorization = "NONE",
+            RestApi = gateway.Id,
+            ResourceId = rootResource.Id,
+            RequestParameters = new InputMap<bool>
+            {
+                { "method.request.path.bucketkey", false },
+            },
+        },  new CustomResourceOptions
+        {
+            DependsOn = { gateway, rootResource },
+            Parent = rootResource,
+        });
+
+        var rootResourceIntegration = new Integration("integration-get", new IntegrationArgs
+        {
+            HttpMethod = "GET",
+            IntegrationHttpMethod = "GET",
+            ResourceId = rootResource.Id,
+            RestApi = gateway.Id,
+            Type = "HTTP_PROXY",
+            Uri = Output.Format($"http://{s3.WebsiteEndpoint}/{{bucketkey}}"),
+            RequestParameters = new InputMap<string>
+            {
+                { "integration.request.path.bucketkey", "method.request.path.bucketkey" },
+            },
+        }, new CustomResourceOptions
+        {
+            DependsOn = { gateway, rootResource, s3 },
+            Parent = rootResource,
         });
 
         var apiResource = new Pulumi.Aws.ApiGateway.Resource("resource-api", new Pulumi.Aws.ApiGateway.ResourceArgs
@@ -134,7 +195,7 @@ class ScalesStack : Stack
 
     private static Role CreateLambdaRole()
     {
-        var lambdaRole = new Role("iam-role", new RoleArgs
+        var lambdaRole = new Role("lambda-role", new RoleArgs
         {
             AssumeRolePolicy = @"{
                 ""Version"": ""2012-10-17"",
@@ -149,7 +210,7 @@ class ScalesStack : Stack
             }"
         });
 
-        _ = new RolePolicy("iam-role-policy", new RolePolicyArgs
+        _ = new RolePolicy("lambda-role-policy", new RolePolicyArgs
         {
             Role = lambdaRole.Id,
             Policy = @"{
@@ -170,6 +231,42 @@ class ScalesStack : Stack
         });
 
         return lambdaRole;
+    }
+
+    private static Role CreateGatewayRole()
+    {
+        var gatewayRole = new Role("api-role", new RoleArgs
+        {
+            AssumeRolePolicy = @"{
+                ""Version"": ""2012-10-17"",
+                ""Statement"": [{
+                    ""Sid"": """",
+                    ""Effect"": ""Allow"",
+                    ""Principal"": {
+                        ""Service"": ""apigateway.amazonaws.com""
+                    },
+                    ""Action"": ""sts:AssumeRole""
+                }]
+            }"
+        });
+
+        _ = new RolePolicy("api-role-policy", new RolePolicyArgs
+        {
+            Role = gatewayRole.Id,
+            Policy = @"{
+                ""Version"": ""2012-10-17"",
+                ""Statement"": [{
+                    ""Effect"": ""Allow"",
+                    ""Action"": [
+                        ""s3:Get*"",
+                        ""s3:List*""
+                    ],
+                    ""Resource"": ""*""
+                }]
+            }"
+        });
+
+        return gatewayRole;
     }
 
     private static string GetApiGatewayPolicy()
@@ -193,6 +290,24 @@ class ScalesStack : Stack
                     ""Sid"": """"
                 }
             ]
+        }";
+    }
+
+    private static string GetS3PublicReadPolicy()
+    {
+        return @"{
+            ""Version"": ""2012-10-17"",
+            ""Statement"": [{
+                ""Sid"": """",
+                ""Effect"": ""Allow"",
+                ""Principal"": ""*"",
+                ""Action"": [
+                    ""s3:GetObject""
+                ],
+                ""Resource"": [
+                    ""arn:aws:s3:::scalesapi/*""
+                ]
+            }]
         }";
     }
 }
