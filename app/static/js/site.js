2311ccd2ad098d69ab87aebdc7ce21c5f563f9b8
Alex Cook <apcook7104@gmail.com>
2020-12-06 16:32:37 -0600

    Add site html, css, and js

--- app/static/js/site.js
+++ app/static/js/site.js
@@ -1,1 +1,16 @@
-// Todo - Add site js
+let scalesDiv = document.getElementById("scales");
+
+fetch("api/scales")
+    .then(result => result.json())
+    .then(jsonResult => Object.keys(jsonResult).forEach(key => {
+        let header = document.createElement("h2");
+        header.innerHTML = key.includes("major") ? "Major" : "Minor";
+        scalesDiv.append(header);
+        
+        let array = jsonResult[key];
+        let para = document.createElement("p");
+        array.forEach(val => {
+            para.innerHTML += val + ", ";
+        });
+        scalesDiv.append(para);
+    }));
