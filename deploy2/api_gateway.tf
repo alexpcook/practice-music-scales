61b137dfcca5d87316733b18da595f50dc6d9e33
Alex Cook <apcook7104@gmail.com>
2021-03-20 15:16:35 -0700

    Add basic API gateway config

---
+++ deploy2/api_gateway.tf
@@ -0,0 +1,9 @@
+resource "aws_api_gateway_rest_api" "scales_api" {
+  name = join("-", [var.name_prefix, "gateway"])
+}
+
+resource "aws_api_gateway_resource" "scales_api" {
+  parent_id   = aws_api_gateway_rest_api.scales_api.root_resource_id
+  path_part   = "{bucketkey}"
+  rest_api_id = aws_api_gateway_rest_api.scales_api.id
+}
