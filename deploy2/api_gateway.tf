3415fad5b56716a2515f9acf2ac59d2f5ae32ef9
Alex Cook <apcook7104@gmail.com>
2021-03-20 15:41:48 -0700

    Add integration with s3 bucket

--- deploy2/api_gateway.tf
+++ deploy2/api_gateway.tf
@@ -17,3 +17,15 @@ resource "aws_api_gateway_method" "scales_api" {
     "method.request.path.bucketkey" = false
   }
 }
+
+resource "aws_api_gateway_integration" "scales_api" {
+  http_method             = aws_api_gateway_method.scales_api.http_method
+  integration_http_method = aws_api_gateway_method.scales_api.http_method
+  resource_id             = aws_api_gateway_resource.scales_api.id
+  rest_api_id             = aws_api_gateway_rest_api.scales_api.id
+  type                    = "HTTP_PROXY"
+  uri                     = "http://${aws_s3_bucket.scales_api_website.website_endpoint}/{bucketkey}"
+  request_parameters = {
+    "integration.request.path.bucketkey" = "method.request.path.bucketkey"
+  }
+}
