68d96a4cb8e30fedfe30af2295b37a65b8b33016
Alex Cook <apcook7104@gmail.com>
2021-03-20 15:54:59 -0700

    Add scales api endpoint

--- deploy2/api_gateway.tf
+++ deploy2/api_gateway.tf
@@ -32,8 +32,14 @@ resource "aws_api_gateway_integration" "scales_s3_site" {
 }
 
 # Lambda function invocation
-resource "aws_api_gateway_resource" "scales_api" {
+resource "aws_api_gateway_resource" "scales_api_base" {
   parent_id   = aws_api_gateway_rest_api.scales_api.root_resource_id
   path_part   = "api"
+  rest_api_id = aws_api_gateway_rest_api.scales_api.id
+}
+
+resource "aws_api_gateway_resource" "scales_api_endpoint" {
+  parent_id   = aws_api_gateway_resource.scales_api_base.id
+  path_part   = "scales"
   rest_api_id = aws_api_gateway_rest_api.scales_api.id
 }
