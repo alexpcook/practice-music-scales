4113f50ff1180b9d87a6b9f41d9a7a74860e4751
Alex Cook <apcook7104@gmail.com>
2021-03-22 18:56:48 -0700

    Handle create and destroy

--- deploy/_deploy.bash
+++ deploy/_deploy.bash
@@ -1,25 +1,38 @@
 #!/bin/bash
 
+action=$1
+
+if [ -z $action ]; then
+    echo "want exactly one argument equal to 'create' or 'destroy'"
+    exit 1
+fi
+
 appDir="../app"
 apiDir="$appDir/api"
 staticDir="$appDir/static"
-
 lambdaExecutable="handler"
-
-# Build Go Lambda function handler...
-cd $apiDir
-GOOS=linux GOARCH=amd64 go build -o $lambdaExecutable "$lambdaExecutable.go"
-zip "$lambdaExecutable.zip" $lambdaExecutable
-rm -f $lambdaExecutable
-
-# Deploy to AWS with Terraform...
-cd -
 
 export TF_VAR_lambda_zip="$apiDir/$lambdaExecutable.zip"
 export TF_VAR_lambda_handler=$lambdaExecutable
 export TF_VAR_public_ip=$(curl -s https://checkip.amazonaws.com)
 export TF_VAR_static_files_dir=$staticDir
 
-terraform fmt || { echo 'Terraform formatting error'; exit 1; }
-terraform validate || { echo 'Terraform validation error'; exit 1; }
-terraform apply || { echo 'Terraform apply error'; exit 1; }
+if [ $action == "create" ]; then
+    # Build Go Lambda function handler...
+    cd $apiDir
+    GOOS=linux GOARCH=amd64 go build -o $lambdaExecutable "$lambdaExecutable.go"
+    zip "$lambdaExecutable.zip" $lambdaExecutable
+    rm -f $lambdaExecutable
+
+    # Deploy to AWS with Terraform
+    cd -
+    terraform fmt || { echo 'Terraform formatting error'; exit 1; }
+    terraform validate || { echo 'Terraform validation error'; exit 1; }
+    terraform apply || { echo 'Terraform apply error'; exit 1; }
+elif [ $action == "destroy" ]; then
+    # Destroy AWS infrastructure
+    terraform destroy || { echo 'Terraform destruction error', exit 1; }
+else 
+    echo "invalid argument: want 'create' or 'destroy', got '$action'"
+    exit 1
+fi
