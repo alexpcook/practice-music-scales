dab5e879a822004e5a4c124d3bb5d0e639c34d14
Alex Cook <apcook7104@gmail.com>
2021-03-27 21:53:16 -0700

    Fix issue with invoke url output

--- aws/serverless/outputs.tf
+++ aws/serverless/outputs.tf
@@ -1,3 +1,3 @@
 output "api_gateway_url" {
-  value = "${aws_api_gateway_deployment.scales_api.invoke_url}/${aws_api_gateway_stage.scales_api.id}/${var.static_files_index_document}"
+  value = "${aws_api_gateway_stage.scales_api.invoke_url}/${var.static_files_index_document}"
 }
