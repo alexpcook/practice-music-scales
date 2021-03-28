24c7905cc7e0f3248494f856281fb54ccc486ed0
Alex Cook <apcook7104@gmail.com>
2021-03-27 17:22:23 -0700

    Add run after building Docker image

--- docker/_deploy.bash
+++ docker/_deploy.bash
@@ -11,5 +11,9 @@ find $staticFilesDir -name site.* | xargs -I % cp % $buildDir
 # Build the Docker image
 docker build --build-arg static_files_dir=$buildDir -t practice-music-scales .
 
-# Clean up
+# Clean up the build stage
 rm -rf $buildDir
+
+# Run the Docker image
+docker run -d -p 8080:80 practice-music-scales
+open http://localhost:8080/site.html
